1cada167c434ef563dfb2dc84e745d3459785fbd
ghale-madayag <ghale.madayag@gmail.com>
2024-05-19 12:38:57 +0800

    update url build

--- resources/js/Pages/Project/Index.vue
+++ resources/js/Pages/Project/Index.vue
@@ -173,11 +173,11 @@
     };
 
     const showURL = (cell) => {
-       router.visit('/project/'+cell+'/')
+       router.visit('/project/'+cell+'/', { preserveScroll: true })
     }
 
     const editUrl = (row) => {
-        router.visit('/project/'+row.cells[6].data+'/edit')
+        router.visit('/project/'+row.cells[6].data+'/edit', { preserveScroll: true })
     }
 
 </script>
