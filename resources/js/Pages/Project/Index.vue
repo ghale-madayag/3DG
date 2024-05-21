0fd16eb84db53ea7c705a1e656f34bf9f013d455
ghale-madayag <ghale.madayag@gmail.com>
2024-05-21 15:42:52 +0800

    Update project index link using full url

--- resources/js/Pages/Project/Index.vue
+++ resources/js/Pages/Project/Index.vue
@@ -173,7 +173,7 @@
     };
 
     const showURL = (cell) => {
-       router.get('/project/'+cell+'/', { preserveScroll: true })
+       router.get('https://app.3dgrealtydevelopment.com/project/'+cell+'/', { preserveScroll: true })
     }
 
     const editUrl = (row) => {
