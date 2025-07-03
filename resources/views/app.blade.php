18f786c8ac2482b57b568c78644e6d1564f341ff
Ghale <ghale.madayag@gmail.com>
2025-07-03 12:35:11 +0800

    V2.1

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,13 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title inertia>3DG Realty Development</title>
     <meta name="description"
-        content="Velzon is a feature-rich admin and dashboard template built with Inertia.js, Vue.js, and Laravel, designed to simplify web application development.">
+        content="Create exceptional real estate developments that enrich communities and exceed expectations.">
     <meta name="keywords"
-        content="Velzon, Inertia.js, Vue.js, Laravel, admin template, dashboard template, web application">
+        content="">
     <meta name="author" content="Themesbrand">
 
     <!-- Social Media Meta Tags -->
-    <meta property="og:title" content="Velzon - Inertia + Vue & Laravel Admin & Dashboard Template">
+    <meta property="og:title" content="3DG Realty Development">
     <meta property="og:description"
         content="Simplify web application development with Velzon, a feature-rich admin and dashboard template built with Inertia.js, Vue.js, and Laravel.">
     <meta property="og:image" content="URL to the template's logo or featured image">
